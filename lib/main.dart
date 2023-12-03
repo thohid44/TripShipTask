bd290041e9ef86557087936d2827cfa98a22da93
thohid44 <thohidislam44@gmail.com>
2023-12-03 15:37:22 +0600

    3 Nov 23 ps 2 bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:uni_links/uni_links.dart';
 
 import 'Utils/localstorekey.dart';
 import 'pages/Home/view/home_screen.dart';
 import 'pages/Login/view/login_screen.dart';
 import 'dart:io';
 
-void main() async{
+void main() async {
   // final GoogleMapsFlutterPlatform platform = GoogleMapsFlutterPlatform.instance;
   // // Default to Hybrid Composition for the example.
   // (platform as GoogleMapsFlutterAndroid).useAndroidViewSurface = true;
-  
+
   HttpOverrides.global = MyHttpOverrides();
-    await GetStorage.init();
+  await GetStorage.init();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final _box = GetStorage();
+class MyApp extends StatefulWidget {
+
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _box = GetStorage();
+
   var result;
-  // This widget is the root of your application.
+
+  StreamSubscription? _sub;
+
+  Future<void> initUniLinks() async {
+    // Attach a listener to th stream
+    _sub = linkStream.listen((String? link) {
+      if (link != null) {
+        print("Listener is working");
+        var uri = Uri.parse("$link");
+        if (uri.queryParameters['token'] != null) {
+          print(uri.queryParameters['token'].toString());
+        }
+      }
+    }, onError: (err) {
+      print(err.toString());
+    });
+  }
+
+  void initState() {
+    super.initState();
+    initUniLinks();
+  }
+
   @override
   Widget build(BuildContext context) {
-   
-
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
         return GetMaterialApp(
-        
           debugShowCheckedModeBanner: false,
           title: 'Trip Ship Task',
           theme: ThemeData(
@@ -51,14 +81,11 @@ class MyApp extends StatelessWidget {
   bool checkUserType() {
     var result1 = _box.read(LocalStoreKey.token.toString());
     if (result1 != null) {
- 
       return true;
     }
 
     return false;
   }
-
-  ///
 }
 
 class MyHttpOverrides extends HttpOverrides {
@@ -69,7 +96,6 @@ class MyHttpOverrides extends HttpOverrides {
           (X509Certificate cert, String host, int port) => true;
   }
 }
-
 
 // import 'package:flutter/material.dart';
 // import 'package:go_router/go_router.dart';
